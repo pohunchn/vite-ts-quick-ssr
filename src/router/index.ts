3c66d35720d9a5efe71731c54ddc2104802f4968
orzi! <1063614727@qq.com>
2022-10-12 19:15:46 +0800

    init

---
+++ src/router/index.ts
@@ -0,0 +1,47 @@
+import { createRouter, createMemoryHistory, createWebHistory } from "vue-router"
+import type { RouteRecordRaw } from 'vue-router'
+
+function getRoutes() {
+	const { routes } = loadRouters();
+	/**
+	 * 如果要对 routes 做一些处理，请在这里修改
+	 */
+	return routes;
+}
+
+
+console.log(import.meta.env.SSR);
+
+const router = createRouter({
+    history: import.meta.env.SSR
+      ? createMemoryHistory('/')
+      : createWebHistory('/'),
+    routes: getRoutes()
+})
+
+// router.beforeEach((to, from, next) => {
+// 	next()
+// })
+
+export default router;
+
+/** 以下代码不要修改 */
+function loadRouters() {
+	const context = import.meta.globEager("../views/**/*.vue");
+    const routes: RouteRecordRaw[] = [];
+
+    Object.keys(context).forEach((key: any) => {
+        if (key === "./index.ts") return;
+		let name = key.replace(/(\.\.\/views\/|\.vue)/g, '');
+		let path = "/" + name.toLowerCase();
+		if (name === "Index") path = "/";
+		routes.push({
+			path: path,
+			name: name,
+			// component: () => import(`../views/${name}.vue`)
+			component: () => context[`../views/${name}.vue`]
+		})
+    });
+
+    return { context, routes }
+}
---
+++ src/views/Index.vue
@@ -0,0 +1,32 @@
+<template>
+	<HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+	<div class="test1"></div>
+	<div class="test2"></div>
+</template>
+
+<style scoped>
+#admin { width: 100%; min-height: 100vh; height: 100vh; }
+.top { height: 20px; }
+
+.main { height: calc(100% - 40px); }
+.main .nav { width: 200px; }
+.main .tabs { width: calc(100% - 200px); }
+
+.test1 {
+	background: #000;
+	height: 1px;
+	width: 300px;
+	transform: scale(1, 0.5);
+}
+
+.test2 {
+	width: 300px;
+	height: 10px;
+	border-bottom: 0.5px solid #000;	
+}
+</style>
+
+<script setup lang="ts">
+import HelloWorld from '@/components/HelloWorld.vue'
+
+</script>
